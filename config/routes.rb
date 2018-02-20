5fb0ae716cf985d6a561032e5bf53ca4bfe95d64
Campus Code 02 <campuscode02@campuss-mini.home>
2018-02-20 20:14:05 -0300

    enable routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
-  resources :recipes
-  resources :cuisines
+  resources :recipes, only: [:show, :new, :create]
+  resources :cuisines, only: [:show, :new, :create]
 end
