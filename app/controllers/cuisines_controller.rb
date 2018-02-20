3f5c0713af30883ab24cb991d1726fe860d0810c
Campus Code 02 <campuscode02@campuss-mini.home>
2018-02-20 19:59:38 -0300

    User register cuisine and must fill in name

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -3,4 +3,21 @@ class CuisinesController < ApplicationController
     id = params[:id]
     @cuisine = Cuisine.find(id)
   end
+
+  def new
+    @cuisine = Cuisine.new
+  end
+
+  def create
+    cuisine_params = params.require(:cuisine).permit(:name)
+    @cuisine = Cuisine.new(cuisine_params)
+
+    if @cuisine.valid?
+      @cuisine.save
+      redirect_to cuisine_path(@cuisine.id)
+    else
+      render :new
+    end
+    # redirect_to root_path
+  end
 end
