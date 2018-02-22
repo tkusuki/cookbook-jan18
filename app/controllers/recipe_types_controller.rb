d03c7de8a643cf16ae5241f1cf778ea50157a92f
Thais e Victor <thais.kusuki@gmail.com>
2018-02-21 22:06:11 -0300

    User register recipe_type

--- app/controllers/recipe_types_controller.rb
+++ app/controllers/recipe_types_controller.rb
@@ -3,4 +3,15 @@ class RecipeTypesController < ApplicationController
     id = params[:id]
     @recipe_type = RecipeType.find(id)
   end
+  def new
+    @recipe_type = RecipeType.new
+  end
+
+  def create
+    recipe_type_name = params[:recipe_type][:name]
+    @recipe_type = RecipeType.new(name: recipe_type_name)
+
+    @recipe_type.save
+    redirect_to @recipe_type
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :recipes, only: [:show, :new, :create]
   resources :cuisines, only: [:show, :new, :create]
-  resources :recipe_types, only: [:show]
+  resources :recipe_types, only: [:show, :new, :create]
 end
