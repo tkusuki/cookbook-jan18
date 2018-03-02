d9d404ef8531d8f92020c5bfafd7101c8aee9268
Thais e Victor <thais.kusuki@gmail.com>
2018-03-01 22:31:27 -0300

    [WIP] User sign in

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 feature 'User sign in' do
   scenario 'successfully' do
     # cria os dados necessários
-    User.create(email: 'user@gmail.com', password: '12345')
+    User.create(email: 'user@gmail.com', password: '12345678')
 
     # simula a ação do usuário
     visit root_path
     click_on 'Entrar'
     fill_in 'Email', with: 'user@gmail.com'
-    fill_in 'Senha', with: '12345'
+    fill_in 'Senha', with: '12345678'
+    click_on 'Logar'
 
     # expectativas do usuário após a ação
     expect(page).to have_css('nav', text: 'Bem-vindo user@gmail.com')
